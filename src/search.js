30036ddc5593caffc850c9e72d9849b05f73578e
陶鸿杰 <18790315365@163.com>
2023-02-11 22:19:05 +0800

    feat: 资源解析，解析 React JSX，lesson 15

---
+++ src/search.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Search extends React.Component {
+  render() {
+    return <div>Search Text</div>;
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,20 @@
 const path = require("path");
 
 module.export = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js",
+  },
   output: {
-    filename: "bundle.js", // 通过占位符确保文件名称唯一
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js", // 通过占位符确保文件名称唯一
   },
 
   module: {
     rules: [
       {
-        test: /\.js$/, // test 指定匹配规则
+        test: /.js$/, // test 指定匹配规则
         use: "babel-loader", // use 指定使用的loader
       },
     ],
